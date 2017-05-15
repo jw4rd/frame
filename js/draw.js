366efe6149b86ac7e2432fbfc70ce25177da847a
jw4rd <j@w4rd.com>
2017-05-14 23:15:50 -0400

    first commit

---
+++ js/draw.js
@@ -0,0 +1,182 @@
+function draw(){
+
+	c = document.getElementById("myCanvas")
+	ctx = c.getContext("2d")
+
+	ctx.canvas.height = window.innerHeight-6
+	ctx.canvas.width = window.innerWidth-6
+
+	ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
+	ctx.lineJoin="round"
+	ctx.lineCap="round"
+
+	ctx.fillStyle="rgba(210,185,160,0.8)"
+	ctx.translate(ctx.canvas.width/2,ctx.canvas.height/2)
+
+	sf=12
+
+	ctx.fillRect(-48*sf,-1.75*sf,96*sf,3.5*sf)
+
+
+	if($("#member").val()=="stud"){
+
+		ctx.beginPath()
+		for(i=0;i<cut1.length;i++){
+			ctx.lineTo((cut1[i].X-47)*sf,(cut1[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+		ctx.beginPath()
+		for(i=0;i<cut2.length;i++){
+			ctx.lineTo( ((cut2[i].X-47) + length ) *sf,(cut2[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+		ctx.strokeStyle="#ff00ff"
+
+		ctx.beginPath()
+		for(i=0;i<cut1toolPath.length;i++){
+			ctx.lineTo((cut1toolPath[i].X-47)*sf,(cut1toolPath[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+		ctx.beginPath()
+		for(i=0;i<cut2toolPath.length;i++){
+			ctx.lineTo( ((cut2toolPath[i].X-47)+length)*sf,(cut2toolPath[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+
+
+	}
+	else if(($("#member").val()=="bottom")||($("#member").val()=="top")){
+
+
+		ctx.beginPath()
+		for(i=0;i<cut0.length;i++){
+			ctx.lineTo((cut0[i].X-47)*sf,(cut0[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+
+		ctx.beginPath()
+		for(i=0;i<cut0.length;i++){
+			ctx.lineTo( ((cut0[i].X-47) + length ) *sf,(cut0[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+		ctx.strokeStyle="#ff00ff"
+
+
+		ctx.beginPath()
+		for(i=0;i<cut0toolPathA.length;i++){
+			ctx.lineTo((cut0toolPathA[i].X-47-tool)*sf,(cut0toolPathA[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+		ctx.beginPath()
+		for(i=0;i<cut0toolPathB.length;i++){
+			ctx.lineTo((cut0toolPathB[i].X-47+tool+length)*sf,(cut0toolPathB[i].Y-1.75)*sf)
+		}
+		ctx.stroke()
+
+		//pockets
+
+		ctx.strokeStyle="#0000ff"
+		ctx.beginPath()
+		for(i=0;i<pocket.length;i++){
+			ctx.lineTo( (pocket[i].X-47+0.75)*sf,(pocket[i].Y)*sf)
+		}
+		ctx.lineTo( (pocket[0].X-47+0.75)*sf,(pocket[0].Y)*sf)
+		ctx.stroke()
+
+		ctx.beginPath()
+		for(i=0;i<pocket.length;i++){
+			ctx.lineTo( (pocket[i].X-47-0.75+length)*sf,(pocket[i].Y)*sf)
+		}
+		ctx.lineTo( (pocket[0].X-47-0.75+length)*sf,(pocket[0].Y)*sf)
+		ctx.stroke()
+
+		n=Math.ceil(length/24)
+
+		for(j=1;j<n;j++){
+			ctx.beginPath()
+			for(i=0;i<pocket.length;i++){
+				ctx.lineTo( (pocket[i].X-47+(length/n*j))*sf,(pocket[i].Y)*sf)
+			}
+			ctx.lineTo( (pocket[0].X-47+(length/n*j))*sf,(pocket[0].Y)*sf)
+			ctx.stroke()
+
+		}
+
+		//pocket toolpath
+
+		//first
+		ctx.strokeStyle="#000"
+		ctx.beginPath()
+		for(i=0;i<pocketToolPathA.length;i++){
+			ctx.lineTo( (pocketToolPathA[i].X-47+0.75)*sf,(pocketToolPathA[i].Y)*sf)
+		}
+		ctx.lineTo( (pocketToolPathA[0].X-47+0.75)*sf,(pocketToolPathA[0].Y)*sf)
+		ctx.stroke()
+
+		//end
+
+		ctx.beginPath()
+		for(i=0;i<pocketToolPathB.length;i++){
+			ctx.lineTo( (pocketToolPathB[i].X-47-0.75+length)*sf,(pocketToolPathB[i].Y)*sf)
+		}
+		ctx.lineTo( (pocketToolPathB[0].X-47-0.75+length)*sf,(pocketToolPathB[0].Y)*sf)
+		ctx.stroke()
+
+		//center pockets 
+
+		n=Math.ceil(length/24)
+
+		for(j=1;j<n;j++){
+			ctx.beginPath()
+			for(i=0;i<pocketToolPath.length;i++){
+				ctx.lineTo( (pocketToolPath[i].X-47+(length/n*j))*sf,(pocketToolPath[i].Y)*sf)
+			}
+			ctx.lineTo( (pocketToolPath[0].X-47+(length/n*j))*sf,(pocketToolPath[0].Y)*sf)
+			ctx.stroke()
+
+		}
+
+		//drill
+		
+		ctx.fillStyle="#ff0000"
+
+		ctx.beginPath()
+		ctx.arc( (drill[0].X-47+0.75)*sf,(drill[0].Y)*sf,tool*sf,0,Math.PI*2)
+		ctx.arc( (drill[1].X-47+0.75)*sf,(drill[1].Y)*sf,tool*sf,0,Math.PI*2)
+		ctx.fill()
+
+		ctx.beginPath()
+		ctx.arc( (drill[0].X-47-0.75+length)*sf,(drill[0].Y)*sf,tool*sf,0,Math.PI*2)
+		ctx.arc( (drill[1].X-47-0.75+length)*sf,(drill[1].Y)*sf,tool*sf,0,Math.PI*2)
+		ctx.fill()
+
+		for(j=1;j<n;j++){
+
+			ctx.beginPath()
+
+			ctx.arc( (drill[0].X-47+(length/n*j))*sf,(drill[0].Y)*sf,tool*sf,0,Math.PI*2)
+			ctx.arc( (drill[1].X-47+(length/n*j))*sf,(drill[1].Y)*sf,tool*sf,0,Math.PI*2)
+
+			ctx.fill()
+
+		}
+		
+
+	}
+
+
+	ctx.strokeStyle="#000"
+	ctx.moveTo(-48.25*sf,1.75*sf)
+	ctx.lineTo(-47.75*sf,1.75*sf)
+	ctx.moveTo(-48*sf,1.5*sf)
+	ctx.lineTo(-48*sf,2*sf)
+	ctx.stroke()
+
+}
---
+++ js/make.js
@@ -0,0 +1,202 @@
+function make(){
+
+		
+		sbp="MS,0.5,0.5\n"
+		sbp+="JZ,2\n"
+		sbp+="C6\n"
+		sbp+="PAUSE 3\n"
+
+		if($("#member").val()=="stud"){
+			
+			part="stud"
+
+			//make cut1 at 1"
+
+			sbp+="J2," + (cut1toolPath[0].X+1) + "," + cut1toolPath[0].Y + "\n"
+
+			sbp+="JZ,-0.8\n"
+
+			for(i=0;i<cut1toolPath.length;i++){
+				sbp+="M2," + (cut1toolPath[i].X+1) + "," + cut1toolPath[i].Y + "\n"
+			}
+
+			sbp+="JZ,-1.6\n"
+
+			for(i=cut1toolPath.length-1;i>=0;i--){
+				sbp+="M2," + (cut1toolPath[i].X+1) + "," + cut1toolPath[i].Y + "\n"
+			}
+
+			sbp+="JZ,2\n"
+
+			//make cut2 at length+1	
+	
+			sbp+="J2," + (cut2toolPath[0].X+1+length) + "," + cut2toolPath[0].Y + "\n"
+
+			sbp+="JZ,-0.8\n"
+
+			for(i=0;i<cut2toolPath.length;i++){
+				sbp+="M2," + (cut2toolPath[i].X+1+length) + "," + cut2toolPath[i].Y + "\n"
+			}
+
+			sbp+="JZ,-1.6\n"
+
+			for(i=cut2toolPath.length-1;i>=0;i--){
+				sbp+="M2," + (cut2toolPath[i].X+1+length) + "," + cut2toolPath[i].Y + "\n"
+			}
+
+		}
+
+		else if(($("#member").val()=="top")||($("#member").val()=="bottom")){
+			
+			part="plateA"
+
+			//cut 1"
+			
+			sbp+="J2," + (cut0toolPathA[0].X+1-tool).toFixed(4) + "," + cut0toolPathA[0].Y + "\n"
+
+			sbp+="JZ,-0.8\n"
+
+			for(i=0;i<cut0toolPathA.length;i++){
+				sbp+="M2," + (cut0toolPathA[i].X+1-tool).toFixed(4) + "," + cut0toolPathA[i].Y + "\n"
+			}
+
+			sbp+="JZ,-1.6\n"
+
+			for(i=cut0toolPathA.length-1;i>=0;i--){
+				sbp+="M2," + (cut0toolPathA[i].X+1-tool).toFixed(4) + "," + cut0toolPathA[i].Y + "\n"
+			}
+
+			sbp+="JZ,0.25\n"
+	
+			//1st pocket
+
+			sbp+="J2," + (pocketToolPathA[0].X+1+0.75) + "," + (pocketToolPathA[0].Y+1.75) + "\n"
+			sbp+="JZ,0.0\n"
+			sbp+="MZ,-0.38\n"
+			for(i=0;i<pocketToolPathA.length;i++){
+				sbp+="M2," + (pocketToolPathA[i].X+1+0.75) + "," + (pocketToolPathA[i].Y+1.75) + "\n"
+			}
+			sbp+="M2," + (pocketToolPathA[0].X+1+0.75) + "," + (pocketToolPathA[0].Y+1.75) + "\n"
+			sbp+="JZ,2\n"
+
+			//stud pockets
+
+			n=Math.ceil(length/24)
+
+			for(j=1;j<n;j++){
+
+				sbp+="J2," + (pocketToolPath[0].X+1+(length/n*j)) + "," + (pocketToolPath[0].Y+1.75) + "\n"
+				sbp+="JZ,0.0\n"
+				sbp+="MZ,-0.38\n"
+				for(i=0;i<pocketToolPath.length;i++){
+					sbp+="M2," + (pocketToolPath[i].X+1+(length/n*j)) + "," + (pocketToolPath[i].Y+1.75) + "\n"
+				}
+				sbp+="M2," + (pocketToolPath[0].X+1+(length/n*j)) + "," + (pocketToolPath[0].Y+1.75) + "\n"
+				sbp+="JZ,2\n"
+
+			}
+
+			//end pocket
+
+			sbp+="J2," + (pocketToolPathB[0].X+1-0.75+length) + "," + (pocketToolPathB[0].Y+1.75) + "\n"
+			sbp+="JZ,0.0\n"
+			sbp+="MZ,-0.38\n"
+			for(i=0;i<pocketToolPathB.length;i++){
+				sbp+="M2," + (pocketToolPathB[i].X+1-0.75+length) + "," + (pocketToolPathB[i].Y+1.75) + "\n"
+			}
+			sbp+="M2," + (pocketToolPathB[0].X+1-0.75+length) + "," + (pocketToolPathB[0].Y+1.75) + "\n"
+			sbp+="JZ,0.25\n"
+
+			//cut end
+
+			sbp+="J2," + (cut0toolPathB[0].X+1+length+tool).toFixed(4) + "," + cut0toolPathB[0].Y + "\n"
+
+			sbp+="JZ,-0.8\n"
+
+			for(i=0;i<cut0toolPathB.length;i++){
+				sbp+="M2," + (cut0toolPathB[i].X+1+length+tool).toFixed(4) + "," + cut0toolPathB[i].Y + "\n"
+			}		
+
+			sbp+="JZ,-1.6\n"
+
+			for(i=cut0toolPathA.length-1;i>=0;i--){
+				sbp+="M2," + (cut0toolPathB[i].X+1+length+tool).toFixed(4) + "," + cut0toolPathB[i].Y + "\n"
+			}
+
+			sbp+="JZ,2\n"
+
+		}
+
+
+		sbp+="JZ,2\n"
+
+		sbp+="C7\n"
+
+		sbp+="J2,60,20\n"
+
+		var link = document.getElementById("downloadLink")
+
+		link.setAttribute("href", "data:text/plain;base64," + btoa(sbp))
+		link.setAttribute("download", part + ".sbp")
+		link.click()
+
+
+		if(($("#member").val()=="top")||($("#member").val()=="bottom")){
+			
+			part="plateB"
+
+			sbp="MS,0.5,0.5\n"
+			sbp+="JZ,2\n"
+			sbp+="C6\n"
+			sbp+="PAUSE 3\n"
+
+			//drill only
+
+			//1st drill
+
+			sbp+="J2," + (drill[0].X+1+0.75) + "," + (drill[0].Y+1.75) + "\n"
+			sbp+="JZ,-0.06\n"
+			sbp+="JZ,0.25\n"
+			sbp+="J2," + (drill[1].X+1+0.75) + "," + (drill[1].Y+1.75) + "\n"
+			sbp+="JZ,-0.06\n"
+			sbp+="JZ,2\n"
+
+			//stud drill
+
+			n=Math.ceil(length/24)
+
+			for(j=1;j<n;j++){
+
+				sbp+="J2," + (drill[0].X+1+(length/n*j)) + "," + (drill[0].Y+1.75) + "\n"
+				sbp+="JZ,-0.06\n"
+				sbp+="JZ,0.25\n"
+				sbp+="J2," + (drill[1].X+1+(length/n*j)) + "," + (drill[1].Y+1.75) + "\n"
+				sbp+="JZ,-0.06\n"
+				sbp+="JZ,2\n"
+
+			}
+
+			//end drill
+
+			sbp+="J2," + (drill[0].X+1-0.75+length) + "," + (drill[0].Y+1.75) + "\n"
+			sbp+="JZ,-0.06\n"
+			sbp+="JZ,0.25\n"
+			sbp+="J2," + (drill[1].X+1-0.75+length) + "," + (drill[1].Y+1.75) + "\n"
+			sbp+="JZ,-0.06\n"
+
+			
+
+			sbp+="JZ,2\n"
+			sbp+="C7\n"
+			sbp+="J2,60,20\n"
+
+			var link = document.getElementById("downloadLink")
+
+			link.setAttribute("href", "data:text/plain;base64," + btoa(sbp))
+			link.setAttribute("download", part + ".sbp")
+			link.click()
+
+		}
+
+
+}
